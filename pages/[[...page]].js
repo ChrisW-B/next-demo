de44fb3287233f46163cacb4feafa7efac6aad46
Chris Barry <2490974+ChrisW-B@users.noreply.github.com>
2021-11-19 13:22:55 -0500

    fake query issue

--- pages/[[...page]].js
+++ pages/[[...page]].js
@@ -1,8 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ page }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -16,8 +16,10 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <p>Page: {page}</p>
+
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -58,12 +60,30 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
+
+export const getStaticPaths = async () => {
+  const paths = [
+    { params: { page: [""] } },
+    { params: { page: ["b"] } },
+    { params: { page: ["c"] } },
+  ];
+
+  return { paths, fallback: "blocking" };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const pageName = params?.page?.[0] || "";
+  return {
+    props: { page: pageName },
+    revalidate: 60 * 2,
+  };
+};
